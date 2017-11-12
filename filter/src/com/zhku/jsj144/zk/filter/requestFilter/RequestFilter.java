d6abf9429c959bae50d41e1f14778c7ab4046cd0
Forever99 <2406045700@qq.com>
2017-11-12 12:54:03 +0800

    过滤器   解决全站的请求数据乱码问题 get请求和post请求完善

--- filter/src/com/zhku/jsj144/zk/filter/requestFilter/RequestFilter.java
+++ filter/src/com/zhku/jsj144/zk/filter/requestFilter/RequestFilter.java
@@ -33,33 +33,27 @@ public class RequestFilter implements Filter {
 		
 		System.out.println("开始工作。。。。。。。。。。。。。。。。");
 		
-//		String method = req.getMethod();//获取请求参数的方法
-//		//get方式请求
-//		if("get".equals(method)){
-			
-			
-			//思路：获取参数有三种方式，我们的目的：就是对这三种方式进行改写：即增强它们的功能
-			//装饰者模式，我们的思路时加强request,然后把加强后的request传递下去，这样，
-			//在使用加强后的request的获得参数的三种方法，就可以获得设置编码后的，正确的中文参数了。
-			
-			//Servlet API 中提供了一个request对象的Decorator设计模式的默认实现类HttpServletRequestWrapper
-			//注意：HttpServletRequestWrapper 类实现了request 接口中的所有方法，
-			//但这些方法的内部实现都是仅仅调用了一下所包装的的 request 对象的对应方法
-			
-			//我们继承：HttpServletRequestWrapper，然后达到自己的目的
-			MyHttpServletRequestWrapper myRequest =new MyHttpServletRequestWrapper(req);
-			
-//			req.setCharacterEncoding("utf-8");
-//			chain.doFilter(req, resp);//放行
-			
-			//增强后的request：myRequest--然后放行
-			chain.doFilter(myRequest, resp);//放行
-//		}
-//		//post方式请求
-//		else{
-//			req.setCharacterEncoding("utf-8");
-//			chain.doFilter(req, resp);//放行
-//		}
+		///解决post中文乱码问题  
+	    req.setCharacterEncoding("UTF-8");  
+        resp.setCharacterEncoding("UTF-8");  
+        resp.setContentType("text/html;charset=UTF-8"); 
+        
+        
+        //解决get中文乱码问题  
+		
+		//思路：获取参数有三种方式，我们的目的：就是对这三种方式进行改写：即增强它们的功能
+		//装饰者模式，我们的思路时加强request,然后把加强后的request传递下去，这样，
+		//在使用加强后的request的获得参数的三种方法，就可以获得设置编码后的，正确的中文参数了。
+		
+		//Servlet API 中提供了一个request对象的Decorator设计模式的默认实现类HttpServletRequestWrapper
+		//注意：HttpServletRequestWrapper 类实现了request 接口中的所有方法，
+		//但这些方法的内部实现都是仅仅调用了一下所包装的的 request 对象的对应方法
+		
+		//我们继承：HttpServletRequestWrapper，然后达到自己的目的
+		MyHttpServletRequestWrapper myRequest =new MyHttpServletRequestWrapper(req);
+		
+		//增强后的request：myRequest--然后放行
+		chain.doFilter(myRequest, resp);//放行
 	}
 
 	
